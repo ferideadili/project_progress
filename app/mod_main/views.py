4d82f0e1bf91c6d4220080f44b1487b7f10c2465
ferideadili <feride_adili@hotmail.com>
2017-05-31 21:28:15 +0200

    listing projects

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -8,15 +8,15 @@ import os, json
 import time
 from datetime import datetime
 from flask.ext.security import current_user
-mod_main = Blueprint('main', __name__)
+mod_main = Blueprint('main', __name__,static_folder="static")
 
 
 @mod_main.route('/', methods=['GET'])
 def index():
+
     return redirect("/en/theme/default")
 
-@mod_main.route('/<lang_code>/theme/<theme>', methods=['GET'])
+@mod_main.route('/<lang_code>/theme/<theme>/', methods=['GET'])
 def root():
-    print g.current_lang
     return render_template('mod_main/index.html',theme=g.current_theme,lang_code=g.current_lang)
 
