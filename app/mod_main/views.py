6d8dd31ac99ce2c345da150f07a8d91c6cf4ba14
ferideadili <feride_adili@hotmail.com>
2017-06-01 18:51:52 +0200

    notes per project

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,g
 # coding=utf-8
 import pprint
-from app import mongo,UPLOAD_FOLDER,ALLOWED_EXTENSIONS,user_utils
+from app import mongo,user_utils,projects
 from bson import json_util, ObjectId
 from flask import Blueprint, render_template, request, Response, redirect, url_for, flash,session
 import os, json
@@ -13,10 +13,16 @@ mod_main = Blueprint('main', __name__,static_folder="static")
 
 @mod_main.route('/', methods=['GET'])
 def index():
-
     return redirect("/en/theme/default")
 
 @mod_main.route('/<lang_code>/theme/<theme>/', methods=['GET'])
 def root():
-    return render_template('mod_main/index.html',theme=g.current_theme,lang_code=g.current_lang)
+    return render_template('mod_main/index.html', theme=g.current_theme, lang_code=g.current_lang,projects=projects)
 
+
+@mod_main.route('/<lang_code>/theme/<theme>/notes/projectId/<int:project_id>', methods=['GET'])
+def notes(project_id):
+    for project in projects:
+        if project["id"] == project_id:
+            target_project = project
+    return render_template('mod_main/notes.html', theme=g.current_theme, lang_code=g.current_lang,project=target_project)
