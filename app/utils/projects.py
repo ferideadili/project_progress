2fc2d35ae7a6f5d59334ad13f2d5ca533bca2fb6
ferideadili <feride_adili@hotmail.com>
2017-06-05 03:41:56 +0200

    single page and vizualization

--- app/utils/projects.py
+++ app/utils/projects.py
@@ -5,6 +5,7 @@ def projects():
         "name":"Project x",
         "description":"Project x description",
         "createdAt":"5/31/2017",
+        #notes
         "notes":
         [
             {
@@ -12,18 +13,180 @@ def projects():
                 "email":"email 0",
                 "text":"text 0",
                 "subject":"subject 0",
-                "images":{
+                "images":[
                     "1.jpg",
                     "2.jpg"
-                }
+                ]
             },
             {   "note_id":2,
                 "email":"email 1",
                 "text":"text 1",
                 "subject":"subject 1"
             }
-        ]
+        ],
+        #end of notes
+        "retail":[
+            {
+                "customer_satisfaction": [
+                    {
+                        "period":"January",
+                        "percentage":30
+
+                    },
+                    {
+                        "period":"February",
+                        "percentage":70
+                    },
+                    {
+                        "period": "March",
+                        "percentage": 20
+                    },
+                    {
+                        "period": "April",
+                        "percentage": 10
+                    },
+                    {
+                        "period": "May",
+                        "percentage": 80
+                    }
+                ],
+                # stock
+                "stock": [
+                    {
+                        "period":"January",
+                        "percentage":40,
+                        "region": [
+                            {
+                                "city": "Obilic",
+                                "amount": 1
+                            },
+                            {
+                                "city": "Prizren",
+                                "amount": 3
+                            },
+                        ]
+                    },
+                    {
+                        "period": "Ferbruary",
+                        "percentage": 50,
+                         "region": [
+                            {
+                                "city": "Obilic",
+                                "amount": 20
+                            },
+                            {
+                                "city": "Prizren",
+                                "amount": 10
+                            },
+                        ]
+                    }
+                ],
+                #end of stock
+                "sales": [
+                    {
+                        "period": "January",
+                        "percentage": 30
+
+                    },
+                    {
+                        "period": "February",
+                        "percentage": 70
+                    },
+                    {
+                        "period": "March",
+                        "percentage": 20
+                    },
+                    {
+                        "period": "April",
+                        "percentage": 10
+                    },
+                    {
+                        "period": "May",
+                        "percentage": 80
+                    }
+                ],
+            }
+        ],
+        #end of retail
+        'healthcare':[
+            {
+            "customer_satisfaction": [
+
+                    {
+                        "period": "January",
+                        "percentage": 50,
+                        "region": [
+                            {
+                                "city": "Obilic",
+                                "amount": 20,
+                                "hospitals":[
+                                    {
+                                        "hospital":"Hospital x",
+                                        "amount":20
+                                    },
+                                    {
+                                        "hospital": "Hospital y",
+                                        "amount": 10
+                                    },
+                                ]
+                            },
+                            {
+                                "city": "Prizren",
+                                "amount": 10,
+                                "hospitals": [
+                                    {
+                                        "hospital": "Hospital x Prizren",
+                                        "amount": 20
+                                    },
+                                    {
+                                        "hospital": "Hospital y Prizren",
+                                        "amount": 10
+                                    },
+                                ]
+                            },
+                        ]
+                    },
+
+                {
+                    "period": "Ferbruary",
+                    "percentage": 20,
+                    "region": [
+                        {
+                            "city": "Obilic",
+                            "amount": 20,
+                            "hospitals": [
+                                {
+                                    "hospital": "Hospital x",
+                                    "amount": 20
+                                },
+                                {
+                                    "hospital": "Hospital y",
+                                    "amount": 10
+                                },
+                            ]
+                        },
+                        {
+                            "city": "Prizren",
+                            "amount": 10,
+                            "hospitals": [
+                                {
+                                    "hospital": "Hospital x Prizren",
+                                    "amount": 20
+                                },
+                                {
+                                    "hospital": "Hospital y Prizren",
+                                    "amount": 10
+                                },
+                            ]
+                        },
+                    ]
+                }#end of january
+
+                ]#end of customer sat
+            }
+        ]#end of healthcare
     },
+    #end of project 1
     {
         "id": 2,
         "name": "Project y",
